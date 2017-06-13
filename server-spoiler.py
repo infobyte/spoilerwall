c37f60c283ed5937a6ac5828b2eb0cbeafc432c3
EzequielTBH <ezequieltbh@infobytesec.com>
2017-06-13 18:17:05 -0300

    Initial commit server-spoiler

---
+++ server-spoiler.py
@@ -0,0 +1,45 @@
+"""
+SpoilerWall
+This service listen in a port and send to the client a SPOILER!!! :P
+
+Use that IPTABLE rule for redirect all traffic incoming from all ports to this service.
+iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 1:65535 -j DNAT --to-destination HOST:PORT
+
+Nmap scan => Spoilers everywhere.
+
+"""
+
+import socket
+import json
+import random
+
+HOST = "127.0.0.1"
+PORT = 8080
+
+FILE_SPOILER = "spoilers.json"
+MAX_SPOILER_COUNT = 4200
+
+fileSpoilers = open(FILE_SPOILER, "r")
+spoilers = json.load(fileSpoilers)
+movies = spoilers['movies']
+fileSpoilers.close()
+
+if __name__ == "__main__":
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST, PORT))
+    s.listen(3)
+    print "Ready for ACTION"
+
+    # Main Loop, listen for a connection ... and spoiler!
+    while 1:
+
+        # Random spoiler select
+        chosen = random.choice(movies)
+        spoiler_chosen = chosen['name'] + '\n' + ' '.join([x['value'] for x in chosen['spoilers']]) + '\n'
+
+        conn, addr = s.accept()
+        print 'Connected with', addr
+
+        conn.sendall(spoiler_chosen.encode('ascii','ignore'))
+        conn.close()
